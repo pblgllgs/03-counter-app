faf9ece8240c67ade47af19cbad0eece6f4cf732
pbl.gllgs <pbl.gllgs@gmail.com>
2021-12-09 00:06:08 -0300

    primeros pasos react

---
+++ src/PrimeraApp.js
@@ -0,0 +1,11 @@
+
+const PrimeraApp = () => {
+  return (
+    <>
+      <h1> Hola mundo </h1>
+      <p>Mi primera app React</p>
+    </>
+  );
+};
+
+export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import PrimeraApp from "./PrimeraApp";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//enlazamos el lugar donde queremos mostrar el mensaje
+const divRoot = document.querySelector("#root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//renderiazamos el mensaje en el lugar que queremos
+ReactDOM.render(<PrimeraApp />, divRoot);
